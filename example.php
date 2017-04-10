2e4faec88b0f26c062e545ec85114c28771fa877
Chien Wei Lin <cwlin0416@gmail.com>
2017-04-10 17:18:24 +0800

    Add example.php and fix load() function

--- example.php
+++ example.php
@@ -9,15 +9,15 @@ use ezsvnaccfile\ACLItemMember;
 try {
 	$file = new SVNAccessFile();
 	$file->load("./svn-access-file");
-	$group = new Group("new-group", array("cwlin", "loln"));
+	$group = new Group("new-group", array("user1", "user2"));
 	$file->addGroup($group);
 
-	$item = new ACLItem("/", "taian_erp2");
+	$item = new ACLItem("/", "repo1");
 	$itemMember = ACLItemMember::withGroup($group, ACLItemMember::PERM_READWRITE);
 	
 	$item->addMember($itemMember);
 	$file->addACLItem($item);
-	$file->save("./svn-access-file.test");
+	$file->save("./svn-access-file");
 } catch(Exception $exc) {
 	echo $exc->getMessage(). "\n";
 }
